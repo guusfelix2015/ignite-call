d844a15bba329ff067afea260963506b82db1014
guusfelix2015 <gusfelix2015@gmail.com>
2024-02-23 18:11:28 -0300

    validação com o zod

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,35 +1,57 @@
-import { Button, TextInput } from '@ignite-ui/react'
-import { Form } from './styles'
+import { Button, TextInput, Text } from '@ignite-ui/react'
+import { Form, FormAnnotation } from './styles'
 import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const claimUsernameFormSchema = z.object({
-  userName: z.string().min(3).max(20),
+  userName: z
+    .string({})
+    .min(3, { message: 'O nome de usuário deve ter no mínimo 3 caracteres' })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: 'O nome de usuário deve conter apenas letras e hifens',
+    })
+    .transform((value) => value.toLowerCase()),
 })
 
 type ClaimUsernameFormSchemaData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUserNameForm() {
-  const { register, handleSubmit } = useForm<ClaimUsernameFormSchemaData>()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ClaimUsernameFormSchemaData>({
+    resolver: zodResolver(claimUsernameFormSchema),
+  })
 
   async function handleClaimUsername(data: ClaimUsernameFormSchemaData) {
     console.log(data)
   }
 
   return (
-    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
-      <TextInput
-        {...register('userName')}
-        size="sm"
-        prefix="ignite.com"
-        placeholder="seu-usuario"
-        crossOrigin="anonymous"
-      />
-      <Button size="sm" type="submit">
-        Reservar usuário
-        <ArrowRight />
-      </Button>
-    </Form>
+    <>
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          {...register('userName')}
+          size="sm"
+          prefix="ignite.com"
+          placeholder="seu-usuario"
+          crossOrigin="anonymous"
+        />
+        <Button size="sm" type="submit">
+          Reservar usuário
+          <ArrowRight />
+        </Button>
+      </Form>
+      <FormAnnotation>
+        <Text size="sm">
+          {errors.userName
+            ? errors.userName.message
+            : 'Escolha um nome de usuário para reservar'}
+        </Text>
+      </FormAnnotation>
+    </>
   )
 }
